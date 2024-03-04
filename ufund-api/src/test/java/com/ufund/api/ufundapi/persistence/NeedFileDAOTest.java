d77d344a76efb88e38111f6cecd8e5c2ff4a187a
rma1399 <rma1399@rit.edu>
2024-03-04 17:48:47 -0500

    fixed public map issue and fixed testing structure

--- ufund-api/src/test/java/com/ufund/api/ufundapi/persistence/NeedFileDAOTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/persistence/NeedFileDAOTest.java
@@ -1,4 +1,4 @@
-package com.persistence;
+package com.ufund.api.ufundapi.persistence;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
