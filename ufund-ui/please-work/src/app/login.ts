cdb514d9dda994136111d5a425160a259801abab
rma1399 <rma1399@rit.edu>
2024-03-24 20:27:15 -0400

    can everyone see the code now

--- ufund-ui/please-work/src/app/login.ts
+++
@@ -1,5 +0,0 @@
-export interface AuthCredentials {
-    username: string;
-    password: string;
-    authLevel: string;
-}
--- ufund-ui/please-work/src/app/needs-checkout.ts
+++
@@ -1,4 +0,0 @@
-export interface NeedCheckout {
-    username: string;
-    checkoutIds: number [];
-}
